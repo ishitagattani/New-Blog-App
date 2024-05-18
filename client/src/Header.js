23130962ba7b2f0c0ac24d083d8d541a5ffd8925
Ishita Gattani <ishitagattani2003@gmail.com>
2024-05-18 13:41:38 +0530

    reorganize code

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,23 +1,24 @@
-import {Link} from "react-router-dom";
-import {useContext, useEffect, useState} from "react";
-import {UserContext} from "./UserContext";
+import { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
+import { API_URL } from "./constants";
 
 export default function Header() {
-  const {setUserInfo,userInfo} = useContext(UserContext);
+  const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch('http://localhost:4000/profile', {
-      credentials: 'include',
-    }).then(response => {
-      response.json().then(userInfo => {
+    fetch(`${API_URL}/profile`, {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
         setUserInfo(userInfo);
       });
     });
-  }, []);
+  }, [setUserInfo]);
 
   function logout() {
-    fetch('http://localhost:4000/logout', {
-      credentials: 'include',
-      method: 'POST',
+    fetch(`${API_URL}/logout`, {
+      credentials: "include",
+      method: "POST",
     });
     setUserInfo(null);
   }
@@ -26,7 +27,9 @@ export default function Header() {
 
   return (
     <header>
-      <Link to="/" className="logo">MyBlog</Link>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
       <nav>
         {username && (
           <>
--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,26 +1,27 @@
-import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css';
-import {useState} from "react";
-import {Navigate} from "react-router-dom";
+import { useState } from "react";
+import "react-quill/dist/quill.snow.css";
+import { Navigate } from "react-router-dom";
 import Editor from "../Editor";
+import { API_URL } from "../constants";
 
 export default function CreatePost() {
-  const [title,setTitle] = useState('');
-  const [summary,setSummary] = useState('');
-  const [content,setContent] = useState('');
-  const [files, setFiles] = useState('');
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
   const [redirect, setRedirect] = useState(false);
   async function createNewPost(ev) {
     const data = new FormData();
-    data.set('title', title);
-    data.set('summary', summary);
-    data.set('content', content);
-    data.set('file', files[0]);
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0]);
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/post', {
-      method: 'POST',
+
+    const response = await fetch(`${API_URL}/post`, {
+      method: "POST",
       body: data,
-      credentials: 'include',
+      credentials: "include",
     });
     if (response.ok) {
       setRedirect(true);
@@ -28,22 +29,25 @@ export default function CreatePost() {
   }
 
   if (redirect) {
-    return <Navigate to={'/'} />
+    return <Navigate to={"/"} />;
   }
   return (
     <form onSubmit={createNewPost}>
-      <input type="title"
-             placeholder={'Title'}
-             value={title}
-             onChange={ev => setTitle(ev.target.value)} />
-      <input type="summary"
-             placeholder={'Summary'}
-             value={summary}
-             onChange={ev => setSummary(ev.target.value)} />
-      <input type="file"
-             onChange={ev => setFiles(ev.target.files)} />
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
       <Editor value={content} onChange={setContent} />
-      <button style={{marginTop:'5px'}}>Create post</button>
+      <button style={{ marginTop: "5px" }}>Create post</button>
     </form>
   );
 }
