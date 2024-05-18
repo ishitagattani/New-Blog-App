82a0052e6f5ca53b33bf8f5314f9ff54db4fff1e
Ishita Gattani <ishitagattani2003@gmail.com>
2024-05-18 21:41:34 +0530

    allow OPTIONS

--- api/src/index.js
+++ api/src/index.js
@@ -20,7 +20,10 @@ const secret = process.env.JWT_SECRET;
 // CORS: allow all
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
